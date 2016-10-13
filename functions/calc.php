cc09146866579c0e39756986cc0cc8c8e36cdfcd
mcatm <tkcs@pelepop.com>
2016-10-13 19:36:04 +0900

    update

--- functions/calc.php
+++ functions/calc.php
@@ -1,6 +1,20 @@
 <?php
 
-function calc($a, $b)
+function calc($a = null, $b = null, $op = 'sum')
 {
-  return $a + $b;
+  if (!$a) throw new Exception;
+  switch ($op)
+  {
+    case 'sum':
+    return $a + $b;
+
+    case 'sub':
+    return $a - $b;
+
+    // case 'mul':
+    // return $a * $b;
+
+    case 'div':
+    return $a / $b;
+  }
 }
--- tests/calc.test.php
+++ tests/calc.test.php
@@ -13,4 +13,26 @@ class CalcTest extends TestCase
     $this->assertEquals( calc(2, 2), 4 );
   }
 
+  public function testSecondArgIsNull()
+  {
+    $this->assertEquals( calc(38), 38 );
+  }
+
+  /**
+   * @expectedException Exception
+   */
+  public function testFirstArgIsNull()
+  {
+    calc();
+  }
+
+  public function testSum()
+  {
+    $this->assertEquals( calc(1, 1), 2 );
+    $this->assertEquals( calc(1, 1, 'sum'), 2 );
+    $this->assertEquals( calc(1, 1, 'sub'), 0 );
+    $this->assertEquals( calc(4, 4, 'mul'), 16 );
+    $this->assertEquals( calc(8, 4, 'div'), 2 );
+  }
+
 }
